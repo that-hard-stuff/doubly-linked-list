ef2ef7e6d56b524c37d92ded1b49fd9f2e9546b7
that-hard-stuff <ilyaivjev2@gmail.com>
2017-01-26 19:39:51 +0300

    implement append, head, tail methods

--- src/linked-list.js
+++ src/linked-list.js
@@ -3,13 +3,41 @@ const Node = require('./node');
 class LinkedList {
     constructor() {
       this.length = 0;
+      this._head = null;
+      this._tail = null;
     }
 
-    append(data) {}
+    append(data) {
+      if (!this.length) {
+        this.length = 0;
+        this._head = new Node(data);
+        this._tail = this._heed;
+        
+        return;
+      }
+        
+      if (this.length === 1) {
+        this.length++;
+        this._tail = new Node(data, this._head);
+        this._head.next = this._tail;
+          
+        return;
+      }
+        
+      this._tail.next = new Node(data, this._tail);
+      this._tail = this._tail.next;
+      this.length++;
+        
+      return;
+    }
 
-    head() {}
+    head() {
+      return this._head.data;
+    }
 
-    tail() {}
+    tail() {
+      return this._tail.data;
+    }
 
     at(index) {}
 
